ee922749c8e78ce53d3f69b3524339845ec3abf8
Vijayroman <42169869+Vijayroman@users.noreply.github.com>
2019-07-02 16:11:40 +0530

    Create large.py

---
+++ large.py
@@ -0,0 +1,9 @@
+d,e,f=map(int,(input().split()))
+if (d > e and d > f):
+          print(d)
+elif (e > d and e > f):
+          print(e)
+elif (f > d and f > e):
+          print(f)
+else:
+          print(d)
